de6fa1b8a68a585e2e541b638b2fcf7b22ea7695
Marco Baringer <marco@onshored.com>
2014-05-29 14:30:11 +0200

    Export commit-logical-transaction and abort-logical-transaction

--- postmodern/package.lisp
+++ postmodern/package.lisp
@@ -24,9 +24,10 @@
    #:sequence-next #:list-sequences #:sequence-exists-p
    #:list-tables #:table-exists-p #:table-description
    #:list-views #:view-exists-p
-   #:*current-logical-transaction* #:with-transaction #:commit-transaction #:abort-transaction
+   #:*current-logical-transaction*
+   #:with-logical-transaction #:ensure-transaction #:abort-logical-transaction #:commit-logical-transaction
+   #:with-transaction #:commit-transaction #:abort-transaction
    #:with-savepoint #:rollback-savepoint #:release-savepoint
-   #:with-logical-transaction #:ensure-transaction
    #:abort-hooks #:commit-hooks
    #:db-null #:coalesce
 
